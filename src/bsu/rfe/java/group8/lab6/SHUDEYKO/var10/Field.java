8f65de4a548ddf82ef7c1f43a1ff0b2618aa6513
Vector <hiuon.x@gmail.com>
2019-12-15 18:42:41 +0300

    UPD: added fields and constructor

--- src/bsu/rfe/java/group8/lab6/SHUDEYKO/var10/Field.java
+++ src/bsu/rfe/java/group8/lab6/SHUDEYKO/var10/Field.java
@@ -1,4 +1,29 @@
 package bsu.rfe.java.group8.lab6.SHUDEYKO.var10;
 
-public class Field {
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.Timer;
+
+public class Field extends JPanel {
+
+    private boolean paused;
+    private ArrayList<BouncingBall> balls = new ArrayList<>(10);
+    private Timer repaintTimer = new Timer(10, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            repaint();
+        }
+    });
+
+    public Field(){
+        setBackground(Color.WHITE);
+        repaintTimer.start();
+    }
+
+    
+
+
 }
