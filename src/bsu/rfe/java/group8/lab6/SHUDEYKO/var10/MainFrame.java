c7a4f065f3cf00035f38eea1a23590826fc27d3f
Vector <hiuon.x@gmail.com>
2019-12-15 19:18:21 +0300

    UPD: (MAINFRAIM) fields; function main

--- src/bsu/rfe/java/group8/lab6/SHUDEYKO/var10/MainFrame.java
+++ src/bsu/rfe/java/group8/lab6/SHUDEYKO/var10/MainFrame.java
@@ -1,4 +1,19 @@
 package bsu.rfe.java.group8.lab6.SHUDEYKO.var10;
 
-public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+
+    private JMenuItem pauseMenuItem;
+    private JMenuItem resumeMenuItem;
+
+    private Field field = new Field();
+
+    public static void main(String[] args){
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
