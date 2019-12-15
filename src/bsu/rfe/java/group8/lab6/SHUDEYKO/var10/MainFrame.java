3ed9c9ab8f2e54b7b2329331b9c083511eb12c4b
Vector <hiuon.x@gmail.com>
2019-12-15 19:21:11 +0300

    UPD: (MF) constructor

--- src/bsu/rfe/java/group8/lab6/SHUDEYKO/var10/MainFrame.java
+++ src/bsu/rfe/java/group8/lab6/SHUDEYKO/var10/MainFrame.java
@@ -1,6 +1,8 @@
 package bsu.rfe.java.group8.lab6.SHUDEYKO.var10;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class MainFrame extends JFrame {
     private static final int WIDTH = 700;
@@ -10,6 +12,51 @@ public class MainFrame extends JFrame {
     private JMenuItem resumeMenuItem;
 
     private Field field = new Field();
+
+    public MainFrame(){
+        super("Программирование и синхронизация потоков");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - WIDTH)/2, (kit.getScreenSize().height - HEIGHT)/2);
+
+        setExtendedState(MAXIMIZED_BOTH);
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu ballMenu = new JMenu("Мячи");
+        Action addBallAction = new AbstractAction("Добавить мяч") {
+            public void actionPerformed(ActionEvent event) {
+                field.addBall();
+                if (!pauseMenuItem.isEnabled() &&
+                        !resumeMenuItem.isEnabled()) {
+                    pauseMenuItem.setEnabled(true);
+                }
+            }
+        };
+        menuBar.add(ballMenu);
+        ballMenu.add(addBallAction);
+        JMenu controlMenu = new JMenu("Управление");
+        menuBar.add(controlMenu);
+        Action pauseAction = new AbstractAction("Приостановить движение"){
+            public void actionPerformed(ActionEvent event) {
+                field.pause();
+                pauseMenuItem.setEnabled(false);
+                resumeMenuItem.setEnabled(true);
+            }
+        };
+        pauseMenuItem = controlMenu.add(pauseAction);
+        pauseMenuItem.setEnabled(false);
+        Action resumeAction = new AbstractAction("Возобновить движение") {
+            public void actionPerformed(ActionEvent event) {
+                field.resume();
+                pauseMenuItem.setEnabled(true);
+                resumeMenuItem.setEnabled(false);
+            }
+        };
+        resumeMenuItem = controlMenu.add(resumeAction);
+        resumeMenuItem.setEnabled(false);
+// Добавить в центр граничной компоновки поле Field
+        getContentPane().add(field, BorderLayout.CENTER);
+    }
 
     public static void main(String[] args){
         MainFrame frame = new MainFrame();
